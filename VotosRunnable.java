d1c8e6f7d4bbfcb254a16ec3461c4bcf74f097da
fsjimenez <92813773+fsjimenez@users.noreply.github.com>
2023-06-01 21:22:25 -0500

    Add files via upload

---
+++ VotosRunnable.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.Random;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+public class VotosRunnable implements Runnable{
+
+    private final Asambleista asambleista;
+    private static final int MAX_DELAY = 10000;
+    private final VotosController insercion = new VotosController();
+
+    public VotosRunnable(Asambleista asambleistas) {
+        this.asambleista = asambleistas;
+
+    }
+
+
+    public void run() {
+
+        //long delay = (long) (Math.random() * MAX_DELAY);
+        try{
+            Thread.sleep(1000);
+        } catch (InterruptedException e){
+            e.getStackTrace();
+        }
+
+        boolean resultado = insercion.addData(asambleista, getTipo());
+
+        if (resultado) {
+            System.out.println("El voto se ha añadido correctamente.");
+        } else {
+            System.out.println("No se pudo añadir el voto.");
+        }
+
+    }
+
+    public String getTipo(){
+        Random random = new Random();
+        String tipoVoto;
+        int randomNumber = random.nextInt(100) + 1;
+        if (randomNumber >= 1 && randomNumber <= 24) {
+            tipoVoto = "S";
+        } else if (randomNumber >= 25 && randomNumber <= 49) {
+            tipoVoto = "N";
+        } else {
+            tipoVoto = "A";
+        }
+        return tipoVoto;
+    }
+
+}
